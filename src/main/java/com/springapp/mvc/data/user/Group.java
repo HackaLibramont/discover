27d48b97deeb2f1eecbd9cc972604fe751a10acf
nigeorges <nicolas.georges.9508@eco.hers.be>
2015-03-20 22:26:06 +0100

    ADD : Group list methods

--- src/main/java/com/springapp/mvc/data/user/Group.java
+++ src/main/java/com/springapp/mvc/data/user/Group.java
@@ -7,7 +7,8 @@ import java.util.List;
 /**
  * Created by Nathan on 20/03/2015.
  */
-public class Group implements Iterable<VirtualUser>{
+public class Group implements Iterable<VirtualUser>
+{
 
     private final List<VirtualUser> users;
 
@@ -17,7 +18,58 @@ public class Group implements Iterable<VirtualUser>{
     }
 
     @Override
-    public Iterator<VirtualUser> iterator() {
+    public Iterator<VirtualUser> iterator()
+    {
         return this.users.iterator();
+    }
+
+    public boolean add(VirtualUser user)
+    {
+        return users.add(user);
+    }
+
+    public void add(int index, VirtualUser user)
+    {
+        users.add(index, user);
+    }
+
+    public boolean contains(VirtualUser user)
+    {
+        return users.contains(user);
+    }
+
+    public VirtualUser get(int index)
+    {
+        return users.get(index);
+    }
+
+    public int indexOf(VirtualUser user)
+    {
+        return users.indexOf(user);
+    }
+
+    public boolean isEmpty()
+    {
+        return users.isEmpty();
+    }
+
+    public boolean remove(VirtualUser user)
+    {
+        return users.remove(user);
+    }
+
+    public VirtualUser remove(int index)
+    {
+        return users.remove(index);
+    }
+
+    public VirtualUser set(int index, VirtualUser user)
+    {
+        return users.set(index, user);
+    }
+
+    public int size()
+    {
+        return users.size();
     }
 }
