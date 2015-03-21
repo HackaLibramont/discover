d0341fe92c687374c79f71a35e6cdd97adbdb668
Administrator <pascal.falbo@hotmail.fr>
2015-03-21 15:33:54 +0100

    abstractDAO

--- src/main/webapp/resources/js/googlemap.js
+++ src/main/webapp/resources/js/googlemap.js
@@ -2,13 +2,17 @@
  * Created by Administrator on 21/03/2015.
  */
 var map;
+var currentWindow;
+var markers = [];
 
 function initialize() {
     var mapOptions = {
         center: { lat: -34.397, lng: 150.644},
         zoom: 8
-    };
+    }
 
+    currentWindow = null;
+    markers = [];
     var origin1 = new google.maps.LatLng(55.930385, -3.118425);
     var destinationB = new google.maps.LatLng(50.087692, 14.421150);
 
@@ -41,7 +45,7 @@ function initialize() {
    /* map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions); */
 
-    routingMap(
+    proximityMap(
         ["55.930385","-3.118425"],
         [
             ["50.087692","14.421150"],
@@ -49,28 +53,37 @@ function initialize() {
         ]);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
-window.onresize=function(){
-
-};
+window.addEventListener("resize", function (event) {
+    var bounds = new google.maps.LatLngBounds();
+    var eltBound = 0;
+    for(i = 0; i < markers.length; i++)
+    {
+        bounds.extend(markers[i].getPosition());
+        eltBound++;
+    }
+    if(eltBound > 1)
+        map.fitBounds(bounds);
+});
 
 function callback(response, status) {
-    if (status == google.maps.DistanceMatrixStatus.OK) {
+    tabResult = [];
+    if (status != google.maps.DistanceMatrixStatus.OK) {
+        alert('Error was: ' + status);
+    }
+    else {
         var origins = response.originAddresses;
         var destinations = response.destinationAddresses;
+        var outputDiv = document.getElementById('outputDiv');
 
         for (var i = 0; i < origins.length; i++) {
             var results = response.rows[i].elements;
             for (var j = 0; j < results.length; j++) {
-                var element = results[j];
-                var distance = element.distance.text;
-                alert(distance);
-                var duration = element.duration.text;
-                alert(duration);
-                var from = origins[i];
-                var to = destinations[j];
+
+                tabResult.push([origins[i],destinations[j],results[j].duration.text]);
             }
         }
     }
+    return tabResult;
 }
 
 function proximityMap(actualLocalisation,tabLocations)
@@ -90,6 +103,7 @@ function proximityMap(actualLocalisation,tabLocations)
         position: actualLoc,
         title:""
     });
+    markers.push(CurrentPositionMarker);
     CurrentPositionMarker.setMap(map);
 
     // map bounds
@@ -106,11 +120,14 @@ function proximityMap(actualLocalisation,tabLocations)
         eltBound++;
     }
 
+    var service = new google.maps.DistanceMatrixService();
+
     for(var i = 0; i < tabLatLng.length; i++)
     {
-        createMarker(tabLatLng[i]);
+        markers.push(createMarker(tabLatLng[i]));
     }
 
+    var markerCluster = new MarkerClusterer(map, markers);
     // if elt = 1 then, only current position so no bounding on the map
     if(eltBound > 1)
         map.fitBounds(bounds);
@@ -135,6 +152,7 @@ function routingMap(actualLocalisation,tabLocations)
         title:""
     });
     CurrentPositionMarker.setMap(map);
+    markers.push(CurrentPositionMarker);
     flightPlanCoordinates.push(actualLoc);
 
     // map bounds
@@ -150,8 +168,6 @@ function routingMap(actualLocalisation,tabLocations)
         bounds.extend(new google.maps.LatLng(tabLocations[i][0],tabLocations[i][1]));
         eltBound++;
     }
-
-
 
     for(var i = 0; i < tabLatLng.length; i++)
     {
@@ -170,7 +186,7 @@ function routingMap(actualLocalisation,tabLocations)
 
     for(var i = 0; i < tabLatLng.length; i++)
     {
-        createMarker(tabLatLng[i]);
+        markers.push(createMarker(tabLatLng[i]));
     }
 
     // if elt = 1 then, only current position so no bounding on the map
@@ -186,13 +202,48 @@ function createMarker(latLng)
         title:""
     });
 
+    var statement = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h1 id="firstHeading" class="firstHeading">Nom activité</h1>'+
+        '<h2 id="firstHeading" class="firstHeading">Type</h2>'+
+        '<div id="bodyContent">'+
+        '<p><b>Totre</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+        'sandstone rock formation in the southern part of the '+
+        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+        'south west of the nearest large town, Alice Springs; 450&#160;km '+
+        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+        'Aboriginal people of the area. It has many springs, waterholes, '+
+        'rock caves and ancient paintings. Uluru is listed as a World '+
+        'Heritage Site.</p>'+
+        '<p><b>3 rue chants doiseaux, 6791, Athus </b></p>' +
+        '<h4>Contact :</h4>'+
+        '<p><a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+        'https://en.wikipedia.org/w/index.php?title=Uluru</a></p> ' +
+        '<p>azerty.7789@hotmail.com</p>'+
+        '<p>+352 691 678 496</p>' +
+        '</div>'+
+        '</div>';
+
     var infowindow = new google.maps.InfoWindow({
-        content: 'lol'
+        content: statement,
+        maxWith: "250px",
+        minWith: "150px"
     });
 
     google.maps.event.addListener(marker, 'click', function() {
+        if(currentWindow != null)
+        {
+            currentWindow.close();
+        }
+        currentWindow = infowindow;
         infowindow.open(map,marker);
     });
 
     marker.setMap(map);
+
+    return marker;
 }
+
